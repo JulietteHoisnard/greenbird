e14f09df7421c4258d4004a4a47b41dc48f6e8af
Rowena Koerber <rowenakoerber@gmail.com>
2020-08-24 17:48:39 +0200

    adding last controller and validations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,7 @@
 class Challenge < ApplicationRecord
   has_many :challenge_users
   has_many :users, through: :challenge_users
+
+  validates :title, :call_to_action, :description, :category, :impact_co, presence: true
+  validates :impact_level, presence: true, inclusion: { in: %w(low, medium, high), message: "Please assign an impact of low, medium or high." }
 end
