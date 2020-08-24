d7abe4da65cb9f322949273064355271ce2419f4
Juliette Hoisnard <juliette@hoisnard.fr>
2020-08-24 17:33:24 +0200

    added users controller with views and dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+  def dashboard
+  end
+
+  def show
+    @challenges = @user.challenges
+  end
+
+  def edit
+  end
+
+  def update
+    if @user == current_user
+      @user.update(user_params)
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :bio, :username, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :challenges
+  resources :users, only: [:show, :edit, :update]
+  get 'users/:id/dashboard', to: 'users#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
