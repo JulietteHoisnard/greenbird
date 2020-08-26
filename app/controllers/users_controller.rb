4fa7d777350bd3a796c381913bfdf26b2d7f1664
Juliette Hoisnard <juliette@hoisnard.fr>
2020-08-26 17:04:20 +0200

    fixed stupid problem with country gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :bio, :username, :photo)
+    params.require(:user).permit(:email, :bio, :username, :country, :photo)
   end
 
   def set_user
