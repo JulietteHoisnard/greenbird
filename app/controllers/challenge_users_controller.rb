dc7af45a6a0acafd2671e03e2c37cace00ce6674
Rowena Koerber <rowenakoerber@gmail.com>
2020-08-27 14:42:27 +0200

    adding link to challenge show page on dashboard

--- app/controllers/challenge_users_controller.rb
+++ app/controllers/challenge_users_controller.rb
@@ -1,2 +1,9 @@
 class ChallengeUsersController < ApplicationController
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    @user = current_user
+    @challenge_user = ChallengeUser.where(user_id: @user.id, challenge_id: @challenge.id)
+    @challenge_user.completed = true
+  end
 end
