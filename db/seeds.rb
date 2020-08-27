b3c8bda7ca2de6785f7a9473ad6bfacdacd3b54b
Tina Wang <tinawang100@gmail.com>
2020-08-27 17:13:33 +0200

    seeds with photos WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,10 @@ one = Challenge.create(
   impact_desc: "air pollution, noise pollution, health, respiratory diseases"
 )
 
+file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533195/greenbird/commutebybike_oockao.jpg')
+one.photo.attach(io: file, filename: 'commutebybike.jpg', content_type: 'image/jpg')
+
+
 puts "Challenge #{one.title} created!"
 
 two = Challenge.create(
@@ -46,12 +50,16 @@ two = Challenge.create(
   impact_desc: "Discover more about vegetarianism"
 )
 
+file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533203/greenbird/eatloweronthefoodchain_rn9vj3.jpg')
+two.photo.attach(io: file, filename: 'eatloweronthefoodchain.jpg', content_type: 'image/jpg')
+
+
 puts "Challenge #{two.title} created!"
 
 three = Challenge.create(
   title: "Reduce Single-Use Plastic",
   call_to_action: "I bring my own bag and avoid plastic.",
-  description: "Plastic is a non-biodegradable material and can take up to 2000 years to decompose. It ends up in oceans and in the stomachs of many animals. Totally getting rid of the use of plastic will save 60-100 million barrels of oil, which is used for its creation, and slow the depletion of this valuable natural resource.",
+  description: "",
   # photo
   link: "https://www.eartheclipse.com/environment/serious-effects-plastic-bags.html",
   category: "live",
@@ -59,17 +67,20 @@ three = Challenge.create(
   infobox_desc: "Environmental benefits but also a lifestyle benefit!",
   infobox_link: "https://www.who.int/globalchange/publications/factsheets/Kit2008_annex1_2.pdf?ua=1",
   impact_level: "medium",
-  impact_co: 100,
+  impact_co: 210,
   impact_money: 100,
   impact_desc: "Discover alternatives to plastic"
 )
+
+file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533220/greenbird/singleuseplastic_rcwbhh.jpg')
+three.photo.attach(io: file, filename: 'reducesingleuseplastic.jpg', content_type: 'image/jpg')
 
 puts "Challenge #{three.title} created!"
 
 four = Challenge.create(
   title: "Laundry",
   call_to_action: "I line-dry my clothes.",
-  description: "Each drying cycle produces over 3 kg of CO2
+  description: "Each drying cycle produces over 3 kg of CO2.
  emissions. One dryer load uses 5 times more electricity than washing – by simply line-drying your clothes, you can save 1/3 of their carbon footprint. Drying clothes naturally is by far the best way to do it: your clothes will last longer and the energy provided is free and non-polluting.",
   # photo
   link: "https://www.huffpost.com/entry/7-instant-ways-to-reduce-your-carbon-footprint_b_59321992e4b00573ab57a383",
@@ -78,10 +89,13 @@ four = Challenge.create(
   infobox_desc: "Small but mighty changes",
   infobox_link: "https://www.theguardian.com/environment/green-living-blog/2010/nov/25/carbon-footprint-load-laundry",
   impact_level: "medium",
-  impact_co: 100,
+  impact_co: 210,
   impact_money: 100,
   impact_desc: "electricity, energy"
 )
+
+file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533212/greenbird/laundry_gerqqj.jpg')
+four.photo.attach(io: file, filename: 'laundry.jpg', content_type: 'image/jpg')
 
 puts "Challenge #{four.title} created!"
 
@@ -100,5 +114,8 @@ five = Challenge.create(
   impact_money: 100,
   impact_desc: "sustainable lifestyle"
 )
+
+file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533216/greenbird/minimalism_tdcdz1.jpg')
+five.photo.attach(io: file, filename: 'minimalism.jpg', content_type: 'image/jpg')
 
 puts "Challenge #{five.title} created!"
