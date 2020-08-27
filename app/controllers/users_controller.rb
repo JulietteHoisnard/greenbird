c50b822d86e507d84836af0918ba12c4c9882234
Rowena Koerber <rowenakoerber@gmail.com>
2020-08-27 15:58:00 +0200

    fixed issue with displaying correct data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def dashboard
-    @challenges_todo = ChallengeUser.where(completed: false)
-    @challenges_done = ChallengeUser.where(completed: true)
+    @user.challenges = Challenge.all
+
+    @challenges_todo = ChallengeUser.where(user_id: current_user.id, completed: false)
+    @challenges_done = ChallengeUser.where(user_id: current_user.id, completed: true)
 
     @challenge = @challenges_todo.first
   end
 
   def show
+    @user.challenges = Challenge.all
     @challenges_done = ChallengeUser.where(completed: true)
     # with the above line you can access and use data from the challenges the user has completed
   end
