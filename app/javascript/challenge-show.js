e5b4ae9d7c24b067cabc539b1ef0c866a9b335f7
Rowena Koerber <rowenakoerber@gmail.com>
2020-08-31 15:35:33 +0200

    adding javascript buttons

--- app/javascript/challenge-show.js
+++
@@ -1,7 +0,0 @@
-
-
-document.querySelector('.info-challenge').addEventListener('mouseover', displayBox);
-
-function displayBox() {
-  document.querySelector('.info-challenge').innerHTML = '<p>"You did it"</p>'
-};
---
+++ app/javascript/packs/challenge-show.js
@@ -0,0 +1,36 @@
+
+
+// document.querySelector('.info-challenge').addEventListener('mouseover', displayBox);
+
+// function displayBox() {
+//   document.querySelector('.info-challenge').innerHTML = '<p>"You did it"</p>'
+// };
+
+const info = document.querySelector('.info-challenge');
+const data = document.querySelector('.data-challenge');
+
+begone(info);
+begone(data);
+
+
+document.querySelector('.infobox').addEventListener('click', (event) => {
+  display(info);
+  begone(document.querySelector('.infobox'));
+  });
+
+document.querySelector('.databox').addEventListener('click', (event) => {
+  display(data);
+  begone(document.querySelector('.databox'));
+});
+
+function hide (element) {
+  element.style.visibility = 'hidden';
+};
+
+function begone (element) {
+  element.style.display = 'none';
+};
+
+function display (element) {
+  element.style.display = '';
+};
