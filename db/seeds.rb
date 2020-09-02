a30b805abd6735f561f23601ea05e2f3a1acfd4c
Rowena Koerber <rowenakoerber@gmail.com>
2020-09-02 11:12:48 +0200

    text changes for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,18 @@ puts "Creating challenges..."
 
 one = Challenge.create!(
   title: "Commute by Bike",
-  call_to_action: "I commute by bike today whether going to work, sports, or shopping, etc.",
-  description: "Riding a bike accounts for about 21g of CO2 emissions per kilometer, which is more than ten times less than a car.",
+  call_to_action: "I commute by bike today whether going to work, sports, or shopping",
+  description: "Grab your bike and cycle to your destination. It's ten times more environmentally friendly than going by car — and you get some fresh air too!",
   # photo
   link: "https://www.sciencedirect.com/science/article/pii/S0048969717301559",
   category: "transport",
-  infobox_title: "Read more about benefits of cycling",
-  infobox_desc: "Envrionmental benefits but not only!",
+  infobox_title: "The benefits of cycling",
+  infobox_desc: "It's good for the environment and your health!",
   infobox_link: "https://ecf.com/news-and-events/news/how-much-co2-does-cycling-really-save",
   impact_level: "medium",
   impact_co: 1000,
   impact_money: 10000,
-  impact_desc: "air pollution, noise pollution, health, respiratory diseases"
+  impact_desc: "Reduces air pollution and noise pollution. Improves health and wards off respiratory diseases"
 )
 
 file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533195/greenbird/commutebybike_oockao.jpg')
@@ -37,17 +37,17 @@ puts "Challenge #{one.title} created!"
 two = Challenge.create(
   title: "Eat Lower on the Food Chain",
   call_to_action: "I will have all vegetarian meals today",
-  description: "Meat lovers have the highest carbon footprint at 3.3 tons of greenhouse gas emissions. A vegan diet has the lowest carbon footprint at just 1.5 tons CO2e (Carbon Dioxide Equivalent). You can reduce your foodprint by a quarter just by cutting down on red meats such as beef and lamb.",
+  description: "Vegetarian meals can be healthier and more cost-effective than meat-based diets, not to mention that meat lovers have a much higher carbon footprint. You can reduce your footprint by a quarter just by cutting down on red meats such as beef and lamb.",
   #photo
   link: "https://www.bbcgoodfood.com/recipes/collection/quick-veggie",
   category: "food",
   infobox_title: "Read more about being a vegetarian",
-  infobox_desc: "There are many different options when it comes to consuming less meat",
+  infobox_desc: "There are many different options when it comes to consuming less meat.",
   infobox_link:"https://www.healthline.com/health/becoming-vegetarian#types",
   impact_level: "medium",
   impact_co: 1325,
   impact_money: 300,
-  impact_desc: "Discover more about vegetarianism"
+  impact_desc: "Lower emissions of greenhouse gases."
 )
 
 file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533203/greenbird/eatloweronthefoodchain_rn9vj3.jpg')
@@ -59,7 +59,7 @@ puts "Challenge #{two.title} created!"
 three = Challenge.create(
   title: "Reduce Single-Use Plastic",
   call_to_action: "I bring my own bag and avoid plastic.",
-  description: "Plastic is a non-biodegradable material and can take up to 2000 years to decompose. It ends up in oceans and in the stomachs of many animals. Totally getting rid of the use of plastic will save 60-100 million barrels of oil and slow the depletion of this valuable natural resource.",
+  description: "Plastic can take up to 2000 years to decompose. In the meantime, it ends up in oceans and in the stomachs of many animals, making them sick. Getting rid of the use of plastic will also save millions of barrels of oil — a valuable natural resource.",
   # photo
   link: "https://www.eartheclipse.com/environment/serious-effects-plastic-bags.html",
   category: "zero waste",
@@ -69,7 +69,7 @@ three = Challenge.create(
   impact_level: "medium",
   impact_co: 210,
   impact_money: 100,
-  impact_desc: "Discover alternatives to plastic"
+  impact_desc: "Reduces pollution and waste, conserves natural resources"
 )
 
 file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533220/greenbird/singleuseplastic_rcwbhh.jpg')
@@ -80,8 +80,7 @@ puts "Challenge #{three.title} created!"
 four = Challenge.create(
   title: "Laundry",
   call_to_action: "I line-dry my clothes.",
-  description: "Each drying cycle produces over 3 kg of CO2.
- emissions. One dryer load uses 5 times more electricity than washing – by simply line-drying your clothes, you can save 1/3 of their carbon footprint. Drying clothes naturally is by far the best way to do it: your clothes will last longer and the energy provided is free and non-polluting.",
+  description: "One dryer load uses 5 times more electricity than washing – by simply line-drying your clothes, you can save 1/3 of their carbon footprint. Drying clothes naturally is by far the best way to do it: your clothes will last longer and the energy provided is free and non-polluting.",
   # photo
   link: "https://www.huffpost.com/entry/7-instant-ways-to-reduce-your-carbon-footprint_b_59321992e4b00573ab57a383",
   category: "home",
@@ -91,7 +90,7 @@ four = Challenge.create(
   impact_level: "medium",
   impact_co: 210,
   impact_money: 100,
-  impact_desc: "electricity, energy"
+  impact_desc: "Reduces electricity usage"
 )
 
 file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533212/greenbird/laundry_gerqqj.jpg')
@@ -102,17 +101,17 @@ puts "Challenge #{four.title} created!"
 five = Challenge.create(
   title: "Minimalism",
   call_to_action: "I will have an open mind towards minimalism.",
-  description: "I will take steps towards living a minimal lifestyle to evaluate, declutter, and practice conscious purchasing.",
+  description: "You can live more sustainably by reducing consumption and using long-lasting items. A great first step to take is to evaluate what you own, declutter, and practice conscious purchasing.",
   # photo
   link: "https://link.springer.com/article/10.1007/s41042-020-00030-y",
   category: "home",
   infobox_title: "Quality over quantity",
-  infobox_desc: "Minimalize Waster with a Minimalist Lifestyle",
+  infobox_desc: "Minimize Waste with a Minimalist Lifestyle",
   infobox_link: "https://minimalismfilm.com/",
   impact_level: "medium",
   impact_co: 100,
   impact_money: 100,
-  impact_desc: "sustainable lifestyle"
+  impact_desc: "Living a more sustainable lifestyle"
 )
 
 file = URI.open('https://res.cloudinary.com/giuliettainthecloud/image/upload/v1598533216/greenbird/minimalism_tdcdz1.jpg')
