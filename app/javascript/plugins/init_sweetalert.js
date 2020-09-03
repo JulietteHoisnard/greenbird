28fee104070f06b8ee5cf9a698209a8a4009fb52
Juliette Hoisnard <66121617+JulietteHoisnard@users.noreply.github.com>
2020-09-03 15:36:46 +0200

    Revert "Landingpage (#84)" (#91)
    This reverts commit 60f157b0df7c58194c47ac6396fa64b9305d242e.

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -16,74 +16,21 @@ const profileBadgePopup = () => {
       .then(response => response.json())
       .then((data) => {
         console.log(data);
-        if (data.challenges_done.length === 1) {
-          console.log("hello")
-        initSweetalertsomethingelse()
-      }});
-};
-
-const profileBadgePopup = () => {
-  const userid = document.querySelector('.p-user')
-  const id = userid.getAttribute('id')
-  fetch(`/users/${id}`, { headers: { accept: "application/json" } })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.challenges_done.length === 3) {
-          console.log("hello")
-        initSweetalertsomethingelse()
-      }});
-};
-
-const profileBadgePopup = () => {
-  const userid = document.querySelector('.p-user')
-  const id = userid.getAttribute('id')
-  fetch(`/users/${id}`, { headers: { accept: "application/json" } })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.challenges_done.length === 5) {
-          console.log("hello")
-        initSweetalertsomethingelse()
-      }});
-};
-
-const profileBadgePopup = () => {
-  const userid = document.querySelector('.p-user')
-  const id = userid.getAttribute('id')
-  fetch(`/users/${id}`, { headers: { accept: "application/json" } })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
         if (data.challenges_done.length === 4) {
           console.log("hello")
         initSweetalertsomethingelse()
       }});
 };
 
-const profileBadgePopup = () => {
-  const userid = document.querySelector('.p-user')
-  const id = userid.getAttribute('id')
-  fetch(`/users/${id}`, { headers: { accept: "application/json" } })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.challenges_done.length === 15) {
-          console.log("hello")
-        initSweetalertsomethingelse()
-      }});
-};
-
-const initSweetalertsomethingelse = () =>  {
+const initSweetalertsomethingelse = () => {
+      document.body.addEventListener("load", function() {
         Swal({
-    title: "AMAZEBALLS",
-    text: "You just achieved a new badge!",
-    icon: "https://media.giphy.com/media/4Tkagznwgrv6A4asQb/giphy.gif",
+    title: "GOLD",
+    text: "You're well on your way to rockstar status!",
+    icon: "https://res.cloudinary.com/giuliettainthecloud/image/upload/c_scale,h_200,w_200/v1599043169/greenbird/Screen_Shot_2018-10-14_at_12.02.24_PM_ae5nzl.png",
     button: "Aww, yes",
     },)
-    }
-;
+    })
+};
 
 export { initSweetalert, profileBadgePopup };
-
-// { document.body.addEventListener("load", function()
